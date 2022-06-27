51438e4c42617bcad2accce8986184789c24f66e
timalsina-yuvraj <timalsinayuvraj@gmail.com>
2022-06-27 12:25:03 +0545

    Front Page with search

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreListingRequest;
 use App\Http\Requests\UpdateListingRequest;
 use App\Models\Listing;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ListingController extends Controller
 {
@@ -13,12 +16,35 @@ class ListingController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $tags = Tag::orderBy('name')->get();
         $listings = Listing::with('tags')->get();
 
-        return view('listings.index', compact('listings'));
+        if ($request->has('s')) {
+            $query = strtolower($request->get('s'));
 
+            $listings = $listings->filter(function ($listing) use ($query) {
+                if (Str::contains(strtolower($listing->title), $query)) {
+                    return true;
+                }
+                if (Str::contains(strtolower($listing->company), $query)) {
+                    return true;
+                }
+                if (Str::contains(strtolower($listing->location), $query)) {
+                    return true;
+                }
+                return false;
+            });
+        }
+        if ($request->has('tag')) {
+            $tag = $request->get('tag');
+            $listings = $listings->filter(function ($listing) use ($tag) {
+                return $listing->tags->contains('slug', $tag);
+            });
+        }
+
+        return view('listings.index', compact('listings', 'tags'));
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::resource('listings',ListingController::class);
+Route::resource('listings', ListingController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
